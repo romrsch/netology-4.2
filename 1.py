e7778c6a7bc8ee6c687235ef4bb5d2228b65b206
Roman Scherbakov <romrsch@gmail.com>
2021-06-27 01:37:28 +0700

    first commit netology 4.2

---
+++ 1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+a = 1
+b = '2'
+
+#c=a+b - Ошибка
+
+c=str(a)+b
+print(c)
+
+c=a+int(b)
+print(c)
+
+
+
+
---
+++ 2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import os
+
+bash_command = ["cd ~/netology/4.2", "git status"]
+
+result_os = os.popen(' && '.join(bash_command)).read()
+
+
+for result in result_os.split('\n'):
+    if result.find('изменено:') != -1:
+        prepare_result = result.replace('\tизменено::   ', '')
+        print(prepare_result)
+
